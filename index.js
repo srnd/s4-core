1f7f7e8d91c2eb02b29f0072e519af3067986b94
TJ Horner <me@tjhorner.com>
2015-09-26 12:52:52 -0700

    fix emitters

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var EventEmitter = require('events'),
+var EventEmitter = require('events').EventEmitter,
     util = require('util');
 
 function Core(){
+  EventEmitter.call(this);
+
+  var _this = this;
+
   this.CONNECTOR_TYPES = {
     TYPE_MESSAGE_SENDER: 1,
     TYPE_MESSAGE_HANDLER: 2
@@ -17,7 +21,7 @@ function Core(){
   this.sendMessage = function(){ };
 
   this.setConnector = function(type, connector){
-    this.emit("connector-loaded", {type: type, connector: connector});
+    _this.emit("connector-loaded", {type: type, connector: connector});
 
     switch(type){
       case this.CONNECTOR_TYPES.TYPE_MESSAGE_SENDER:
@@ -57,9 +61,9 @@ function Core(){
           var args = message.split(" ");
           try{
             c.action(message, args, channel, username, extra);
-            this.emit("command-fired", {command: c}) // You shouldn't use the emmiter to capture data from commands.
+            _this.emit("command-fired", {command: c}) // You shouldn't use the emmiter to capture data from commands.
           }catch(e){
-            this.emit("error", {error: e, channel: channel, command: c});
+            console.log("s4 Core error:", {error: e, channel: channel, command: c});
           }
         }
       });
@@ -70,30 +74,28 @@ function Core(){
             command = true;
             var matches = new RegExp(t.trigger).exec(message);
             t.action(message, matches, channel, username, extra);
-            this.emit("trigger-fired", {trigger: t}); // You shouldn't use the emmiter to capture data from triggers.
+            _this.emit("trigger-fired", {trigger: t}); // You shouldn't use the emmiter to capture data from triggers.
           }
         });
       }
 
       if(!command){
-        this.emit("unknown-response", {message: message, channel: channel, username: username, extra: extra});
+        _this.emit("unknown-response", {message: message, channel: channel, username: username, extra: extra});
       }
     }
   }
 
   this.addCommand = function(trigger, help, action){
     this.commands.push({trigger: trigger, action: action, help: help});
-    this.emit("command-added", {trigger: trigger, action: action, help: help});
+    _this.emit("command-added", {trigger: trigger, action: action, help: help});
     return this.commands;
   }
 
   this.addTrigger = function(trigger, action){
     this.triggers.push({trigger: trigger, action: action});
-    this.emit("trigger-added", {trigger: trigger, action: action});
+    _this.emit("trigger-added", {trigger: trigger, action: action});
     return this.triggers;
   }
-
-  EventEmitter.call(this);
 
   return this;
 }
